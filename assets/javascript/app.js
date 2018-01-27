d48164704e27c9119bd3861b6b5e745b4030aeb9
Alberto Vargas <a.vargasmarte@gmail.com>
2018-01-27 10:44:25 -0500

    added click event to perform ajax call weather API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,32 @@
 // connecting to the weather API
 
-// we create a constant variable with a sample URL for Carrboro NC
-const weatherQueryURL = "http://samples.openweathermap.org/data/2.5/weather?zip=27510,us&appid=324cf8afb8064251c24b6f3c51561345";
+// we create a weatherKey
+const weatherKey = "c890f4972862b62b5b669b4cd555a436";
 
-// we now make the ajax call with the 'GET' method
-$.ajax({
-    url: weatherQueryURL,
-    method: 'GET'
-}).done(function(response) {
-    console.log(response);
+// we declare a variable zipcode that will later store user's zipcode input
+let zipcode;
+
+let weatherQueryURL;
+// we create an on click event for when user submits form
+$("#submit").click(function(event) {
+    event.preventDefault();
+
+    // get inputs
+    zipcode = $("#zip-input").val().trim();
+
+    // we declare a variable holding the URL corresponding to the zipcode input
+    weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",us&appid=" + weatherKey;
+
+    console.log(weatherQueryURL);
+    // we now make the ajax call with the 'GET' method
+    $.ajax({
+        url: weatherQueryURL,
+        method: 'GET'
+    }).done(function(response) {
+        console.log(response);
+    });    
 });
+
+
+
+
